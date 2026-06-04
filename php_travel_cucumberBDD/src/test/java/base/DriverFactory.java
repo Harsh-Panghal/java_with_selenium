00dc23a7af977627f35ede0e93418972a1511b5b
Harsh Choudhary <harsh.choudhary.glb@gmail.com>
2026-06-04 09:48:53 +0530

    update the jenkins file

--- php_travel_cucumberBDD/src/test/java/base/DriverFactory.java
+++ php_travel_cucumberBDD/src/test/java/base/DriverFactory.java
@@ -2,6 +2,7 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import java.time.Duration;
@@ -14,7 +15,15 @@ public class DriverFactory {
         System.out.println("Browser value is: " + browser);
 
         if (browser.equalsIgnoreCase("chrome")) {
-            tlDriver.set(new ChromeDriver());
+            System.out.println("🚀 Running in Headless Mode (Docker Safe)!");
+            ChromeOptions options = new ChromeOptions();
+            
+            options.addArguments("--headless=new"); 
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            
+            tlDriver.set(new ChromeDriver(options));
+            
         } else if (browser.equalsIgnoreCase("firefox")) {
             tlDriver.set(new FirefoxDriver());
         } else if (browser.equalsIgnoreCase("edge")) {
